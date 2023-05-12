d43da83b815dd2bfc2dede7ee422bd4d9279835a
success310 <victory.clever310@gmail.com>
2023-05-12 08:38:36 +0200

    I fixed color problem

--- src/components/Headers/HeaderAdmin.svelte
+++ src/components/Headers/HeaderAdmin.svelte
@@ -1,8 +1,12 @@
 <script>
+  let isMobileSubMenuOpen = false;
+  let isDark = false;
 </script>
 
 <header class="relative bg-white dark:bg-darker">
-  <div class="flex items-center justify-between p-2 border-b dark:border-primary-darker">
+  <div
+    class="flex items-center justify-between p-2 border-b dark:border-primary-darker"
+  >
     <!-- Mobile menu button -->
     <!-- Brand -->
     <a
@@ -13,11 +17,82 @@
     </a>
 
     <!-- Mobile sub menu button -->
-    
+    <button
+      on:click={() => (isMobileSubMenuOpen = !isMobileSubMenuOpen)}
+      class="p-1 transition-colors duration-200 rounded-md text-primary-lighter bg-primary-50 hover:text-primary hover:bg-primary-100 dark:hover:text-light dark:hover:bg-primary-dark dark:bg-dark md:hidden focus:outline-none focus:ring"
+    >
+      <span class="sr-only">Open sub manu</span>
+      <span aria-hidden="true">
+        <svg
+          class="w-8 h-8"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+          />
+        </svg>
+      </span>
+    </button>
 
     <!-- Desktop Right buttons -->
+    <nav
+      aria-label="Secondary"
+      class="hidden space-x-2 md:flex md:items-center"
+    >
+      <!-- Toggle dark theme button -->
+      <button
+        aria-hidden="true"
+        class="relative focus:outline-none"
+        on:click={() => isDark = !isDark}
+      >
+        <div
+          class="w-12 h-6 transition rounded-full outline-none bg-primary-100 dark:bg-primary-lighter"
+        />
+        <div
+          class="{isDark ? 'translate-x-6 text-primary-100 bg-primary-darker' : 'translate-x-0 -translate-y-px  bg-white text-primary-dark'} absolute top-0 left-0 inline-flex items-center justify-center w-6 h-6 transition-all duration-150 transform scale-110 rounded-full shadow-sm"
+        >
+          {#if isDark}
+            <svg
+              class="w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+              />
+            </svg>
+          {:else}
+            <svg
+              class="w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              />
+            </svg>
+          {/if}
+        </div>
+      </button>
+    </nav>
     <!-- User dropdown menu -->
-        
+
     <!-- Mobile sub menu -->
   </div>
   <!-- Mobile main manu -->
