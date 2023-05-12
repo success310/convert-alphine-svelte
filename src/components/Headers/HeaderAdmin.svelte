5cc4da1da8792a795c5855e24d6d98c7e2deeef9
success310 <victory.clever310@gmail.com>
2023-05-12 13:09:51 +0200

    header part of navigation of main content

--- src/components/Headers/HeaderAdmin.svelte
+++ src/components/Headers/HeaderAdmin.svelte
@@ -1,6 +1,10 @@
 <script>
+  let userMenu;
   let isMobileSubMenuOpen = false;
   let isDark = false;
+  let open = false;
+  
+  const profileAvatar = '../assets/img/avatar.jpg';
 </script>
 
 <header class="relative bg-white dark:bg-darker">
@@ -49,13 +53,15 @@
       <button
         aria-hidden="true"
         class="relative focus:outline-none"
-        on:click={() => isDark = !isDark}
+        on:click={() => (isDark = !isDark)}
       >
         <div
           class="w-12 h-6 transition rounded-full outline-none bg-primary-100 dark:bg-primary-lighter"
         />
         <div
-          class="{isDark ? 'translate-x-6 text-primary-100 bg-primary-darker' : 'translate-x-0 -translate-y-px  bg-white text-primary-dark'} absolute top-0 left-0 inline-flex items-center justify-center w-6 h-6 transition-all duration-150 transform scale-110 rounded-full shadow-sm"
+          class="{isDark
+            ? 'translate-x-6 text-primary-100 bg-primary-darker'
+            : 'translate-x-0 -translate-y-px  bg-white text-primary-dark'} absolute top-0 left-0 inline-flex items-center justify-center w-6 h-6 transition-all duration-150 transform scale-110 rounded-full shadow-sm"
         >
           {#if isDark}
             <svg
@@ -90,10 +96,127 @@
           {/if}
         </div>
       </button>
-    </nav>
-    <!-- User dropdown menu -->
 
-    <!-- Mobile sub menu -->
+      <!-- Notification button -->
+      <button
+        class="p-2 transition-colors duration-200 rounded-full text-primary-lighter bg-primary-50 hover:text-primary hover:bg-primary-100 dark:hover:text-light dark:hover:bg-primary-dark dark:bg-dark focus:outline-none focus:bg-primary-100 dark:focus:bg-primary-dark focus:ring-primary-darker"
+      >
+        <span class="sr-only">Open Notification panel</span>
+        <svg
+          class="w-7 h-7"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+          />
+        </svg>
+      </button>
+
+      <!-- Search button -->
+      <button
+        class="p-2 transition-colors duration-200 rounded-full text-primary-lighter bg-primary-50 hover:text-primary hover:bg-primary-100 dark:hover:text-light dark:hover:bg-primary-dark dark:bg-dark focus:outline-none focus:bg-primary-100 dark:focus:bg-primary-dark focus:ring-primary-darker"
+      >
+        <span class="sr-only">Open search panel</span>
+        <svg
+          class="w-7 h-7"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
+
+      <!-- Settings button -->
+      <button
+        class="p-2 transition-colors duration-200 rounded-full text-primary-lighter bg-primary-50 hover:text-primary hover:bg-primary-100 dark:hover:text-light dark:hover:bg-primary-dark dark:bg-dark focus:outline-none focus:bg-primary-100 dark:focus:bg-primary-dark focus:ring-primary-darker"
+      >
+        <span class="sr-only">Open settings panel</span>
+        <svg
+          class="w-7 h-7"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        </svg>
+      </button>
+
+      <div class="relative">
+        <button
+          on:click="{() => { open = !open; if(open){ userMenu.focus(); } } }"
+          type="button"
+          aria-haspopup="true"
+          aria-expanded="{open ? 'true' : 'false'}"
+          class="transition-opacity duration-200 rounded-full dark:opacity-75 dark:hover:opacity-100 focus:outline-none focus:ring dark:focus:opacity-100"
+        >
+          <span class="sr-only">User menu</span>
+          <img class="w-10 h-10 rounded-full" src="{profileAvatar}" alt="Ahmed Kamel" />
+        </button>
+
+        <!-- User dropdown menu -->
+        {#if open}
+        <div
+          bind:this={userMenu}
+          on:click.away="{() => open = false}"
+          on:keydown.escape="{() => open = false}"
+          class="transform transition-all absolute right-0 w-48 py-1 bg-white rounded-md shadow-lg top-12 ring-1 ring-black ring-opacity-5 dark:bg-dark focus:outline-none"
+          tabindex="-1"
+          role="menu"
+          aria-orientation="vertical"
+          aria-label="User menu"
+        >
+          <a
+            href="#profile"
+            role="menuitem"
+            class="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-primary"
+          >
+            Your Profile
+          </a>
+          <a
+            href="#menuitem"
+            role="menuitem"
+            class="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-primary"
+          >
+            Settings
+          </a>
+          <a
+            href="#logout"
+            role="menuitem"
+            class="block px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-100 dark:text-light dark:hover:bg-primary"
+          >
+            Logout
+          </a>
+        </div>
+        {/if}
+      </div>
+    </nav>
   </div>
-  <!-- Mobile main manu -->
 </header>
