e6d03fec1dc3d33390db10f28315c53540559093
success310 <victory.clever310@gmail.com>
2023-05-14 09:01:09 +0200

    loading effect

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <!-- App.svelte -->
 <script>
+  import Loader from './components/Loader.svelte';;
   import SettingsDrawer from "./components/Drawers/SettingsDrawer.svelte";
   import SearchResultsDrawer from "./components/Drawers/SearchResultsDrawer.svelte";
   import NotificationsDrawer from "./components/Drawers/NotificationsDrawer.svelte";
@@ -11,10 +12,20 @@
   export let url = "";
 </script>
 
-<SettingsDrawer />
-<SearchResultsDrawer />
-<NotificationsDrawer />
+<Loader this={() => import('./layouts/Admin.svelte')}>
+  <div
+    slot="fallback"
+    class="fixed inset-0 z-50 flex items-center justify-center text-2xl font-semibold text-white bg-primary-darker"
+  >
+    Loading.....
+  </div>
+</Loader>
+
 <Router url="{url}">
   <!-- admin layout -->
   <Route path="/*admin" component="{Admin}" />
 </Router>
+
+<SettingsDrawer />
+<SearchResultsDrawer />
+<NotificationsDrawer />
